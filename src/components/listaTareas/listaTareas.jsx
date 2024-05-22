9f9adbb4614e030ab358f6ea531f3ff61a871cf2
lusgro <lucasurielrosenbaum@gmail.com>
2024-05-22 13:57:28 -0300

    Borrar Tarea

--- src/components/listaTareas/listaTareas.jsx
+++ src/components/listaTareas/listaTareas.jsx
@@ -1,7 +1,21 @@
+import { useContext } from 'react';
 import './listaTareas.css';
 
 const ListaTareas = () => {
-    return <ul id='listaTareas' />
+    var className
+    const { tareas, setTareas } = useContext(TareasContext);
+    return (
+        tareas.map((tarea, index) => {
+            className = ''
+            if (tarea.fueCompletada) {
+                className = 'completada'
+            }
+            <div>
+                <li className={className}>{tarea.nombre}</li>
+                
+            </div>
+
+
 }
 
 export default ListaTareas;
